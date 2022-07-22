7749bb8a0b180672d3c7cb873c98e7517c2d97fb
ATUL KUMAR <48856680+atulkumar6556@users.noreply.github.com>
2022-07-22 17:41:08 +0530

    Update saveCam1.py

--- saveCam1.py
+++ saveCam1.py
@@ -1,15 +1,12 @@
 import os
 import time
 # handling CONFIGURATION file for BOX setting
-file = "/home/pi/Downloads/*.mp4"
-FILENAME = "/home/pi/Videos/CompressedVideo/"
+file = "your current file to compress"
+FILENAME = "file compressed "
 
 
 def video_compressor(file, FILENAME):
-    # OUTPUTFILE=f'{APP_FOLDER_SERVICE_PATH}/{FILENAME}'
     os.system(f"ffmpeg -i {file} -vcodec h264 -crf 35 -b:v 2097k -acodec aac {FILENAME}")
-    # os.system(f"rm- rf {file}")
-    # os.system(f"cp {OUTPUTFILE} {file}")
     time.sleep(2)
     print("old deleted")
 
